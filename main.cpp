569b5f40b2745cd1a17c0987b76aa674b0e49d10
Unknown <guillaume.priou@utbm.fr>
2018-06-04 11:31:39 +0200

    Ajout pseudo code algo

--- main.cpp
+++ main.cpp
@@ -1,3 +1,40 @@
+/*
+        Pseudo-code algorithme
+
+        nbCreneaux = 4
+
+        Start:
+            var meilleureSolutionActuelle = tab[11]
+            var nouvelleSolution = tab[11]
+
+            var efficaciteMeilleureSolutionActuelle = tab[11]
+            var efficaciteNouvelleSolution = tab[11]
+
+            var incompatiblites = tab[11][11]
+
+            incompatibilites = {
+                                ...
+                                }
+
+
+            meilleureSolutionActuelle = randomSolution()
+            efficaciteMeilleureSolutionActuelle = evaluerSolution (meilleureSolutionActuelle)
+
+            while (sommeDesCases(efficaciteMeilleureSolutionActuelle) > 0)
+                ameliorerSolution ()
+
+        ameliorerSolution:
+            var sessionLaPlusConflictuelle = efficaciteMeilleureSolutionActuelle.maxConflit()
+
+            nouvelleSolution = meilleureSolutionActuelle
+            nouvelleSolution[sessionLaPlusConflictuelle] = rand()%nbCreneaux
+
+            efficaciteNouvelleSolution = evaluerSolution (nouvelleSolution)
+
+            if (efficaciteNouvelleSolution < efficaciteMeilleureSolutionActuelle)
+                meilleureSolutionActuelle = nouvelleSolution
+*/
+
 #include <iostream>
 
 using namespace std;
